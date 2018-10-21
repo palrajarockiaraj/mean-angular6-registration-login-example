c4d0e1cb0a4f80afd6369427f325a61bff58543a
Palraj Arockiaraj <paul.arockiyam@gmail.com>
2018-10-21 13:20:21 +0530

    Removes unwanted files

--- server/test.js
+++
@@ -1,6 +0,0 @@
-var db = require('mongoskin').db('mongodb://localhost:27017/animals');
-
-db.collection('mamals').find().toArray(function(err, result) {
-  if (err) throw err;
-  console.log(result);
-});
